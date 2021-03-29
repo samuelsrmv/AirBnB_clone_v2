7167de9d37f1ff35b4c83939484a63a66d0610e4
josevelez08 <jose-0919@hotmail.com>
2021-03-29 19:46:10 +0000

    the pep8 test for the console file

---
+++ tests/test_console.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python3
+"""model unitest for base model"""
+from console import HBNBCommand
+import unittest
+import pep8
+
+
+class TestConsole(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.Checker("console.py", source_code=True)
+        result = pep8style.check_all()
+        self.assertEqual(result, 0)
+
+if __name__ == '__main__':
+    unittest.main()
