659900e8d6516f4c0fdf43d6c3bc04876b68fd3b
samuelsrmv <samuel.srmv@gmail.com>
2021-03-29 15:00:20 -0500

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- configuration database
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = PASSWORD('hbnb_test_pwd');
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
