8e1bb70c68f543f3a5289c7ce82231398470a79a
samuelsrmv <samuel.srmv@gmail.com>
2021-03-30 22:01:51 -0500

    update user

--- models/user.py
+++ models/user.py
@@ -7,8 +7,8 @@ import models
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    __tablename__ = 'users'
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', pString(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=True)
+    last_name = Column('last_name', String(128), nullable=True)
