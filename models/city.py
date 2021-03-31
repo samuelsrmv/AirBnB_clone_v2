f0ac8c25e3bb5fdb5d51c1add9c5a07b09238bee
samuelsrmv <samuel.srmv@gmail.com>
2021-03-30 21:32:31 -0500

    update city

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 import models
 
 class City(BaseModel, Base):
