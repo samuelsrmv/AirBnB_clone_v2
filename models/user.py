ea9ee31ef5e42236795c9d74b5c6e7db57b86dd5
josevelez08 <jose-0919@hotmail.com>
2021-03-31 03:26:21 +0000

    user

--- models/user.py
+++ models/user.py
@@ -9,15 +9,8 @@ import models
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-<<<<<<< HEAD
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    reviews = relationship('Review', cascade='all, delete', backref='user'))
-=======
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=True)
-    last_name = Column('last_name', String(128), nullable=True)
->>>>>>> 4033205a7fd1dbd8e06b7c446bc3899a8f4cc48a
+    places = relationship('Place', cascade='all, delete', backref='user')
