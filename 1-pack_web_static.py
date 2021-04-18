e9ad98c5a37d8ebee5157b64babecb4f0f66f185
samuelsrmv <samuel.srmv@gmail.com>
2021-04-18 13:21:56 -0500

    update

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Compress before sending"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """script that generates a .tgz archive"""
+    try:
+        now = datetime.now()
+        local('mkdir -p versions/')
+        dt_string = now.strftime("%Y%m%d%H%M%S")
+        filename = "web_static_" + dt_string + ".tgz"
+        local('tar -cvzf versions/{} web_static/'.format(filename))
+        path = './versions/{}'.format(filename)
+        print("web_static packed: {} ->{}Bytes".format(path, os.path.getsize(path)))
+    except:
+        return None
+if __name__ == "__main__":
+    do_pack()
