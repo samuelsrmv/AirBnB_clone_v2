ea3374a1867f5b57d8e3d6f3a7d917cd7453c787
samuelsrmv <samuel.srmv@gmail.com>
2021-05-01 11:41:43 -0500

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """0-hello_route"""
-
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route("/")
 app.url_map.strict_slashes = False
 
+
+@app.route("/")
 def function():
     """function"""
-    return "¡Hola HBNB!"
+    return ("¡Hola HBNB!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
