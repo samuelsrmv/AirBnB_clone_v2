05702c577476fb8526314334c99160d2c51eabdc
samuelsrmv <samuel.srmv@gmail.com>
2021-05-01 11:45:01 -0500

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,20 +4,19 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route("/")
 app.url_map.strict_slashes = False
 
+
+@app.route("/")
 def function():
     """function"""
     return "¡Hola HBNB!"
 
-@app.route("/hbnb")
-app.url_map.strict_slashes = False
 
+@app.route("/hbnb")
 def function2():
     """function2"""
-    return "HBNB"
+    return ("HBNB")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
