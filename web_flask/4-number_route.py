4d1b272080ac45036eeea25bc53b1e3514b9decd
samuelsrmv <samuel.srmv@gmail.com>
2021-05-01 11:59:58 -0500

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""1-hbnb"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def function():
+    """function"""
+    return "¡Hola HBNB!"
+
+
+@app.route("/hbnb")
+def function2():
+    """function2"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def function3(text):
+    """function2"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def function4(text):
+    """function2"""
+    if text is None:
+        return "Python is cool"
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def function_new(n):
+    """function2"""
+    return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
