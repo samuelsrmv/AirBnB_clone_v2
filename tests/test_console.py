c6f6dc9c66251e62f8aeba27cda9ca33f975a770
samuelsrmv <samuel.srmv@gmail.com>
2021-05-02 15:16:46 -0500

    console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """model unitest for base model"""
 from console import HBNBCommand
@@ -6,13 +5,13 @@ import unittest
 import pep8
 
 
-#class TestConsole(unittest.TestCase):
-#
-#    def test_pep8_conformance(self):
-#        """Test that we conform to PEP8."""
-#        pep8style = pep8.Checker("console.py", source_code=True)
-#        result = pep8style.check_all()
-#        self.assertEqual(result, 0)
-#
-#if __name__ == '__main__':
-#    unittest.main()
+class TestConsole(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.Checker("console.py", source_code=True)
+        result = pep8style.check_all()
+        self.assertEqual(result, 0)
+
+if __name__ == '__main__':
+    unittest.main()
